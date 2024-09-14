37e3f1fb155261efbaa8ffe596fefc0522bd02dc
Shay Patel <shay@shay.services>
2024-09-14 14:16:37 +0100

    Added Clustering tools - centroid, euclidean distance, inertia, WCSS

---
+++ src/AIH/Clustering.hs
@@ -0,0 +1,33 @@
+module AIH.Clustering (
+    centroid,
+    distanceEucSquared,
+    distanceEuc,
+    inertia,
+    wcss
+) where 
+
+import Data.List
+
+centroid :: (Fractional a) => [[a]] -> [a]
+centroid xs =
+    let numValues = fromIntegral (length xs)
+        numComponents = fromIntegral (length $ head xs)
+        transposedValues = transpose xs
+        componentAverages = map (\componentList -> sum componentList / numValues) transposedValues
+    in
+        componentAverages
+
+distanceEucSquared :: (Num a, Floating a) => [a] -> [a] -> a
+distanceEucSquared xs ys = sum $ zipWith (\a b -> (a - b) ^ 2) xs ys
+
+distanceEuc :: (Num a, Floating a) => [a] -> [a] -> a
+distanceEuc xs ys = sqrt $ distanceEucSquared xs ys
+
+inertia :: (Floating a) => [[a]] -> a
+inertia xs =
+    let mCentroid = centroid xs
+    in
+        sum $ map (distanceEucSquared mCentroid) xs
+
+wcss :: (Floating a) => [[[a]]] -> a
+wcss xs = sum $ map inertia xs
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,14 @@
 import AIH.Loss
+import AIH.Clustering
 
 main :: IO()
 main = do
     print (maeLoss [1,2,3] [1,6,3])
     print (mseLoss [1,2,3] [1,6,3])
     print (zeroOneLoss [1,2,3] [1,6,3])
+
+    let clusteringStructure = [[[1,2],[22,3],[3,4]],[[64,5],[5,6],[6,7]]]
+    print (centroid (head clusteringStructure))
+    print (distanceEuc [1,2,3] [2,4,6])
+    print (inertia (head clusteringStructure))
+    print (wcss clusteringStructure)
