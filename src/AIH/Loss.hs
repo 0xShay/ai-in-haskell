191016fd85fffd87b8761db9803e40078965a565
James <jameskowal10@gmail.com>
2024-09-15 07:58:52 +0100

    Refactor loss functions

--- src/AIH/Loss.hs
+++ src/AIH/Loss.hs
@@ -1,14 +1,20 @@
 module AIH.Loss (
-    maeLoss,
-    mseLoss,
-    zeroOneLoss
-) where 
+  maeLoss,
+  mseLoss,
+  zeroOneLoss,
+) where
+
+normLoss :: (Fractional c, Num a) => (a -> c) -> [a] -> [a] -> c
+normLoss f xs ys =
+  let mean = (/ fromIntegral (length xs))
+      err = zipWith (-) xs ys
+   in mean . sum . map f $ err
 
 maeLoss :: (Num a, Fractional a) => [a] -> [a] -> a
-maeLoss xs ys = (\n -> n / fromIntegral (length xs)) $ sum $ map abs $ zipWith (-) xs ys
+maeLoss = normLoss abs
 
 mseLoss :: (Num a, Fractional a) => [a] -> [a] -> a
-mseLoss xs ys = (\n -> n / fromIntegral (length xs)) $ sum $ map (\n -> n * n) $ zipWith (-) xs ys
+mseLoss = normLoss (^ 2)
 
 zeroOneLoss :: (Eq a) => [a] -> [a] -> Integer
 zeroOneLoss xs ys = sum $ zipWith (\a b -> if a == b then 0 else 1) xs ys
