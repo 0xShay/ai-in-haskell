54759aac5ccbffa6f8d554f727279e3d670039dc
James <jameskowal10@gmail.com>
2024-11-07 10:59:05 +0000

    Minor clustering fix

--- src/AIH/Clustering.hs
+++ src/AIH/Clustering.hs
@@ -1,33 +1,31 @@
 module AIH.Clustering (
-    centroid,
-    distanceEucSquared,
-    distanceEuc,
-    inertia,
-    wcss
-) where 
+  centroid,
+  distanceEucSquared,
+  distanceEuc,
+  inertia,
+  wcss,
+) where
 
 import Data.List
 
 centroid :: (Fractional a) => [[a]] -> [a]
 centroid xs =
-    let numValues = fromIntegral (length xs)
-        numComponents = fromIntegral (length $ head xs)
-        transposedValues = transpose xs
-        componentAverages = map (\componentList -> sum componentList / numValues) transposedValues
-    in
-        componentAverages
+  let numValues = fromIntegral (length xs)
+      numComponents = fromIntegral (length $ head xs)
+      transposedValues = transpose xs
+      componentAverages = map (\componentList -> sum componentList / numValues) transposedValues
+   in componentAverages
 
 distanceEucSquared :: (Num a, Floating a) => [a] -> [a] -> a
-distanceEucSquared xs ys = sum $ zipWith (\a b -> (a - b) ^ 2) xs ys
+distanceEucSquared xs ys = sum $ zipWith ((-) . (^ 2)) xs ys
 
 distanceEuc :: (Num a, Floating a) => [a] -> [a] -> a
 distanceEuc xs ys = sqrt $ distanceEucSquared xs ys
 
 inertia :: (Floating a) => [[a]] -> a
 inertia xs =
-    let mCentroid = centroid xs
-    in
-        sum $ map (distanceEucSquared mCentroid) xs
+  let mCentroid = centroid xs
+   in sum $ map (distanceEucSquared mCentroid) xs
 
 wcss :: (Floating a) => [[[a]]] -> a
 wcss xs = sum $ map inertia xs
